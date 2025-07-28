dc6c46f7d256137235126af370e087532a64adc9
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2025-07-28 20:37:22 +0300

    feat: refactor Header and DashboardLayout components for improved structure and styling

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,6 +1,5 @@
 import clariantLogo from "../../assets/clariant-logo-small.svg";
 import eptLogo from "../../assets/eptlogo.png"
-import { FaSignOutAlt } from "react-icons/fa";
 import { useAuth } from "../../context/AuthContext";
 
 const Header = () => {
@@ -12,16 +11,9 @@ const Header = () => {
 
       <div className="flex items-center gap-6">
         <div className="flex flex-col items-end">
-          <img src={clariantLogo} alt="Clariant Logo" className="w-12 h-12 mb-1" />
-          <img src={eptLogo} alt="EPT Logo" className="w-12 h-12" />
+          <img src={clariantLogo} alt="Clariant Logo" className="w-5 h-5 mb-1" />
+          <img src={eptLogo} alt="EPT Logo" className="w-20 h-20" />
         </div>
-        <button
-          onClick={() => setUser(null)}
-          className="text-red-600 hover:text-red-800"
-          title="Logout"
-        >
-          <FaSignOutAlt size={24} />
-        </button>
       </div>
     </div>
   );
--- frontend/src/layouts/DashboardLayout.tsx
+++ frontend/src/layouts/DashboardLayout.tsx
@@ -1,16 +1,26 @@
 import { useAuth } from "../context/AuthContext";
 import Header from "../components/Header/Header";
+import { FaSignOutAlt } from "react-icons/fa";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
 
   return (
     <div className="p-6">
       <Header />
-      <h1 className="text-2xl font-bold mt-6 mb-2 flex justify-start">
-        EPT Digital Solution Apps
-      </h1>
-      <h2 className="text-xl font-semibold mt-6 mb-4 flex justify-start">Welcome, {user?.name}</h2>
+      <div className="flex items-center justify-between mt-6 mb-2">
+        <h1 className="text-2xl font-bold">EPT Digital Solution Apps</h1>
+        <button
+          onClick={() => setUser(null)}
+          className="text-red-600 hover:text-red-800"
+          title="Logout"
+        >
+          <FaSignOutAlt size={24} />
+        </button>
+      </div>
+      <h2 className="text-xl font-semibold mt-6 mb-4 flex justify-start">
+        Welcome, {user?.name}
+      </h2>
       {children}
     </div>
   );
