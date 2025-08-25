17bb729e05065a9151b7a2c6745047e292bcabe9
Bonface Njuguna <bonfacenjuguna13@gmail.com>
2025-08-25 21:48:09 +0300

    feat: update redirectUri in msalConfig to use environment variable for production compatibility

--- frontend/src/utils/msalConfig.ts
+++ frontend/src/utils/msalConfig.ts
@@ -2,6 +2,6 @@ export const msalConfig = {
   auth: {
     clientId: import.meta.env.VITE_MS_CLIENT_ID!,
     authority: `https://login.microsoftonline.com/${import.meta.env.VITE_MS_TENANT_ID}`,
-    redirectUri: "http://localhost:5173/",
+    redirectUri: import.meta.env.VITE_MS_REDIRECT_URI!,
   },
 };
